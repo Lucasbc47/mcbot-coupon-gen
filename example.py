ae519f52f2fe35e5089b13d4953ce83768fbd7fc
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-06-29 08:16:58 -0300

    Correção example.py
    Corrigido código de exemplo.

--- example.py
+++ example.py
@@ -1,15 +1,22 @@
 import json
+import os
+
+from selenium.webdriver.chrome.service import Service
 from bot.mcbot import MCBot
 
 with open('config.json', 'r') as file:
     config = json.load(file)
 
 bot = MCBot(
-    executable_path=config["diretorio_webdriver"],
     hidden=config["esconder_webdriver"],
     country=config["country"],
     cnpj_mc=config["cnpj_mc_donalds"],
     quantia=config["quantia"]
 )
+
+if os.name == 'posix': ## for linux
+    bot.executable_path=config["diretorio_webdriver"]
+if os.name == "nt": ## correction for windows
+    bot.service = Service(config["diretorio_webdriver"])
 
 bot.start_gen()
