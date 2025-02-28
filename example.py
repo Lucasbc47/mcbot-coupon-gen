56d71930a6f768f26a9a89c3a30eb9c33bf2d95d
Lucas Costa <122161095+Lucasbc47@users.noreply.github.com>
2025-02-27 21:32:15 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,22 +1,22 @@
 import json
 import os
-
 from selenium.webdriver.chrome.service import Service
 from bot.mcbot import MCBot
 
 with open('config.json', 'r') as file:
     config = json.load(file)
 
+if os.name == 'nt':  # Windows
+    service = Service(executable_path=config["diretorio_webdriver"])
+else:  # Linux/Mac
+    service = Service(executable_path=config["diretorio_webdriver"])
+
 bot = MCBot(
+    service=service,
     hidden=config["esconder_webdriver"],
     country=config["country"],
     cnpj_mc=config["cnpj_mc_donalds"],
     quantia=config["quantia"]
 )
-
-if os.name == 'posix': ## for linux
-    bot.executable_path=config["diretorio_webdriver"]
-if os.name == "nt": ## correction for windows
-    bot.service = Service(config["diretorio_webdriver"])
 
 bot.start_gen()
